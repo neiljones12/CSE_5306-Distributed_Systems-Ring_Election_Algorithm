e88412d2383bd1531a2f1be88dc9832ab65df8f0
Neil Jones <neil.j.neil@gmail.com>
2017-03-25 02:17:20 -0500

    Logical updates

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -26,175 +26,79 @@ app.factory('socket', function ($rootScope) {
 });
 
 //Controller definition 
-app.controller('Controller', function ($scope, $http, $localStorage, $location, socket) {
+app.controller('Controller', function ($scope, $http, $localStorage, $location,$window, socket) {
 
-    $scope.data = [];
-    $scope.message = [];
-    $scope.startMessage = false;
+
+    var sort_by = function (field, reverse, primer) {
+
+        var key = primer ?
+            function (x) { return primer(x[field]) } :
+            function (x) { return x[field] };
+
+        reverse = !reverse ? 1 : -1;
+
+        return function (a, b) {
+            return a = key(a), b = key(b), reverse * ((a < b) - (b < a));
+        }
+    }
+
+    $scope.init = function () {
+        $scope.data = [];
+        $scope.message = [];
+        $scope.startMessage = true;
+        $scope.logMessage = [];
+        $scope.start = true;
+        $scope.client = false;
+        if ($location.port() == 3000) {
+            $scope.client = true;
+        };
+    };
     //test data
-    $scope.data = [{ process: 1, online: true }, { process: 2, online: true }, { process: 3, online: true }, { process: 4, online: true }, { process: 5, online: true }, { process: 6, online: true }];
+    // $scope.data = [{ process: 1, online: true }, { process: 2, online: true }, { process: 3, online: true }, { process: 4, online: true }, { process: 5, online: true }, { process: 6, online: true }];
 
-    socket.on('process_1_connection', (data) => {
-        var isExist = false;
+    socket.on('process_connection', (data) => {
+        var check = true;
         for (var i = 0; i < $scope.data.length; i++) {
-            if ($scope.data[i].process != undefined && $scope.data[i].process == 1) {
-                isExist = true;
+            if ($scope.data[i].process == data.process) {
+                check = false;
+                $scope.data[i].online = data.online;
+                $scope.data[i].closed = data.closed;
             }
         }
 
-        if (!isExist) {
+        if (check) {
             $scope.data.push({
-                process: 1,
-                online: data,
-                successor: 2,
+                process: data.process,
+                online: data.online,
                 isLeader: false,
-                current: false
+                current: false,
+                closed: data.closed
             });
         }
-        else {
-            for (var i = 0; i < $scope.data.length; i++) {
-                if ($scope.data[i].process != undefined && $scope.data[i].process == 1) {
-                    $scope.data[i].online = data
-                }
-            }
-        }
-    });
-
-    socket.on('process_2_connection', (data) => {
-        var isExist = false;
-        for (var i = 0; i < $scope.data.length; i++) {
-            if ($scope.data[i].process != undefined && $scope.data[i].process == 2) {
-                isExist = true;
-            }
-        }
-
-        if (!isExist) {
-            $scope.data.push({
-                process: 2,
-                online: data,
-                successor: 3,
-                isLeader: false,
-                current: false
-            });
-        }
-        else {
-            for (var i = 0; i < $scope.data.length; i++) {
-                if ($scope.data[i].process != undefined && $scope.data[i].process == 2) {
-                    $scope.data[i].online = data
-                }
-            }
-        }
-    });
-
-    socket.on('process_3_connection', (data) => {
-        var isExist = false;
-        for (var i = 0; i < $scope.data.length; i++) {
-            if ($scope.data[i].process != undefined && $scope.data[i].process == 3) {
-                isExist = true;
-            }
-        }
-
-        if (!isExist) {
-            $scope.data.push({
-                process: 3,
-                online: data,
-                successor: 4,
-                isLeader: false,
-                current: false
-            });
-        }
-        else {
-            for (var i = 0; i < $scope.data.length; i++) {
-                if ($scope.data[i].process != undefined && $scope.data[i].process == 3) {
-                    $scope.data[i].online = data
-                }
-            }
-        }
-    });
-
-    socket.on('process_4_connection', (data) => {
-        var isExist = false;
-        for (var i = 0; i < $scope.data.length; i++) {
-            if ($scope.data[i].process != undefined && $scope.data[i].process == 4) {
-                isExist = true;
-            }
-        }
-
-        if (!isExist) {
-            $scope.data.push({
-                process: 4,
-                online: data,
-                successor: 5,
-                isLeader: false,
-                current: false
-            });
-        }
-        else {
-            for (var i = 0; i < $scope.data.length; i++) {
-                if ($scope.data[i].process != undefined && $scope.data[i].process == 4) {
-                    $scope.data[i].online = data
-                }
-            }
-        }
-    });
-
-    socket.on('process_5_connection', (data) => {
-        var isExist = false;
-        for (var i = 0; i < $scope.data.length; i++) {
-            if ($scope.data[i].process != undefined && $scope.data[i].process == 5) {
-                isExist = true;
-            }
-        }
-
-        if (!isExist) {
-            $scope.data.push({
-                process: 5,
-                online: data,
-                successor: 6,
-                isLeader: false,
-                current: false
-            });
-        }
-        else {
-            for (var i = 0; i < $scope.data.length; i++) {
-                if ($scope.data[i].process != undefined && $scope.data[i].process == 5) {
-                    $scope.data[i].online = data
-                }
-            }
-        }
-    });
-
-    socket.on('process_6_connection', (data) => {
-        var isExist = false;
-        for (var i = 0; i < $scope.data.length; i++) {
-            if ($scope.data[i].process != undefined && $scope.data[i].process == 6) {
-                isExist = true;
-            }
-        }
-
-        if (!isExist) {
-            $scope.data.push({
-                process: 6,
-                online: data,
-                successor: 1,
-                isLeader: false,
-                current: false
-            });
-        }
-        else {
-            for (var i = 0; i < $scope.data.length; i++) {
-                if ($scope.data[i].process != undefined && $scope.data[i].process == 6) {
-                    $scope.data[i].online = data
-                }
-            }
-        }
+        
+        console.log(data);
     });
 
     $scope.start = function () {
-        $scope.startMessage = true;
-        $scope.counter = 0;
-        $scope.displayCurrent($scope.counter);
 
+        $scope.startMessage = false;
+        $scope.start = false;
+
+        var max = 0;
+        for (var i = 0; i < $scope.data.length; i++) {
+            if ($scope.data[i].online) {
+                $scope.logMessage.push($scope.data[i].process + " sends message to " + $scope.data[i].successor);
+                if ($scope.data[i].process > max) {
+                    max = $scope.data[i].process;
+                }
+            }
+        }
+        $scope.data[max - 1].isLeader = true;
+        $scope.logMessage.push("Process " + max + " is chosen as the leader as it is the largest");
+    };
+
+    $scope.stop = function () {
+        $scope.start = true;
     };
 
     $scope.displayCurrent = function (id) {
@@ -204,6 +108,32 @@ app.controller('Controller', function ($scope, $http, $localStorage, $location, 
             currentProcess: id,
             successor: $scope.data[id].successor
         });
+    };
+
+    $scope.crash = function (id) {
+        for (var i = 0; i < $scope.data.length; i++) {
+            if ($scope.data[i].process == id) {
+                $scope.data[i].online = false;
+                socket.emit('test');
+            }
+        }
+    };
+
+    $scope.restart = function (id) {
+        for (var i = 0; i < $scope.data.length; i++) { 
+            if ($scope.data[i].process == id) {
+                if ($scope.data[i].closed)
+                {
+                    var port = "300" + id;
+                    var url = "http://localhost:"+port+"/";
+                    $window.open(url);
+                }
+                else
+                {
+                    $scope.data[i].online = true; 
+                }
+            }
+        }
     };
 
 });
--- utils/routes.js
+++ utils/routes.js
@@ -3,10 +3,7 @@ class Routes {
 
     constructor(app, socket) {
         this.app = app;
-        this.io = socket;
-        this.name = "";
-        this.users = [];
-        this.connection = [];
+        this.io = socket; 
 
         //process Ports
         this.process_1 = 3001;
@@ -33,7 +30,7 @@ class Routes {
 
     }
 
-    socketEvents() { 
+    socketEvents() {
         var socket = this.io;
 
         var process1 = this.app.listen(this.process_1);
@@ -41,54 +38,164 @@ class Routes {
 
         socket1.on('connect', function (data) {
             console.log("process 1 connected");
-            socket.emit('process_1_connection', true);
-        }); 
 
-        socket1.on('disconnect', function (data) {
-            console.log("process 1 disconnect");
-            socket.emit('process_1_connection', false);
-        }); 
+            var obj = {
+                process: 1,
+                online: true,
+                closed: false
+            };
+
+            socket.emit('process_connection', obj);
+
+            data.on('disconnect', function (data) {
+                console.log("process 1 disconnect");
+
+                var obj = {
+                    process: 1,
+                    online: false,
+                    closed: true
+                };
+
+                socket.emit('process_connection', obj);
+            });
+        });
+
+
 
         var process2 = this.app.listen(this.process_2);
         var socket2 = require('socket.io').listen(process2);
-        
+
         socket2.on('connect', function (data) {
             console.log("process 2 connected");
-            socket.emit('process_2_connection', true);
+
+            var obj = {
+                process: 2,
+                online: true,
+                closed: false
+            };
+
+            socket.emit('process_connection', obj);
+
+            data.on('disconnect', function (data) {
+                console.log("process 2 disconnect");
+                
+                var obj = {
+                    process: 2,
+                    online: false,
+                    closed: true
+                };
+
+                socket.emit('process_connection', obj);
+            });
         });
 
         var process3 = this.app.listen(this.process_3);
         var socket3 = require('socket.io').listen(process3);
-        
+
         socket3.on('connect', function (data) {
             console.log("process 3 connected");
-            socket.emit('process_3_connection', true);
+            
+            var obj = {
+                process: 3,
+                online: true,
+                closed: false
+            };
+
+            socket.emit('process_connection', obj);
+
+            data.on('disconnect', function (data) {
+                console.log("process 3 disconnect");
+                
+                var obj = {
+                    process: 3,
+                    online: false,
+                    closed: true
+                };
+
+                socket.emit('process_connection', obj);
+            });
         });
 
         var process4 = this.app.listen(this.process_4);
         var socket4 = require('socket.io').listen(process4);
-        
+
         socket4.on('connect', function (data) {
             console.log("process 4 connected");
-            socket.emit('process_4_connection', true);
+            
+            var obj = {
+                process: 4,
+                online: true,
+                closed: false
+            };
+
+            socket.emit('process_connection', obj);
+
+            data.on('disconnect', function (data) {
+                console.log("process 4 disconnect");
+                
+                var obj = {
+                    process: 4,
+                    online: false,
+                    closed: true
+                };
+
+                socket.emit('process_connection', obj);
+            });
         });
 
         var process5 = this.app.listen(this.process_5);
         var socket5 = require('socket.io').listen(process5);
-        
+
         socket5.on('connect', function (data) {
             console.log("process 5 connected");
-            socket.emit('process_5_connection', true);
+            
+            var obj = {
+                process: 5,
+                online: true,
+                closed: false
+            };
+
+            socket.emit('process_connection', obj);
+
+            data.on('disconnect', function (data) {
+                console.log("process 5 disconnect");
+                
+                var obj = {
+                    process: 5,
+                    online: false,
+                    closed: true
+                };
+
+                socket.emit('process_connection', this.processes);
+            });
         });
 
         var process6 = this.app.listen(this.process_6);
         var socket6 = require('socket.io').listen(process6);
-        
+
         socket6.on('connect', function (data) {
             console.log("process 6 connected");
-            socket.emit('process_6_connection', true);
-        });
 
+            var obj = {
+                process: 6,
+                online: true,
+                closed: false
+            };
+
+            socket.emit('process_connection', obj);
+
+            data.on('disconnect', function (data) {
+                console.log("process 6 disconnect");
+                
+                var obj = {
+                    process: 6,
+                    online: false,
+                    closed: true
+                };
+
+                socket.emit('process_connection', obj);
+            });
+        });
     }
 
     routesConfig() {
